0bfe5aa37d107cff564ee4c3343874e067c7a34c
Veronika Angyalová <veronika.angyalova@studio.unibo.it>
2026-02-10 17:14:56 +0100

    feat: setup project configuration

---
+++ artifact/source/config.py
@@ -0,0 +1,9 @@
+import os
+
+# screen constants
+SCREEN_HEIGHT = 600
+SCREEEN_WIDTH = 1100
+
+# dynamic path calculation
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.adspath(__file__)))
+VISUALS_DIR = os.path.join(BASE_DIR, "visuals")
