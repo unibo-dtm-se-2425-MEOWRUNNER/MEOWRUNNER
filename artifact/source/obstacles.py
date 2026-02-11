6f8174409260fd7164886ba7d8890aa5752f67c0
Veronika Angyalová <veronika.angyalova@studio.unibo.it>
2026-02-11 11:17:36 +0100

    feat: add class obstacle

---
+++ artifact/source/obstacles.py
@@ -0,0 +1,14 @@
+class Obstacle:
+    def __init__(self, image, type):
+        self.image = image
+        self.type = type
+        self.rect = self.image[self.type].get_rect()
+        self.rect.x = SCREEN_WIDTH
+
+    def update (self, game_speed, obstacles):
+        self.rect.x -= game_speed
+        if self.rect.x <- self.rect.width:
+            obstacles.pop()
+    
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image[self.type], self.rect)
