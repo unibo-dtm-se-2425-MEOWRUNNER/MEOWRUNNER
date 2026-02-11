1294ad04b5b821b537affcdf87fa65544626a185
Veronika Angyalová <veronika.angyalova@studio.unibo.it>
2026-02-11 14:06:55 +0100

    feat: add obstacle classes

--- artifact/source/obstacles.py
+++ artifact/source/obstacles.py
@@ -12,3 +12,29 @@ class Obstacle:
     
     def draw(self, SCREEN):
         SCREEN.blit(self.image[self.type], self.rect)
+
+
+class Rock(Obstacle):
+    def __init__(self, image, type):
+        self.type = random.randint(0,2)
+        super().__init__(image, self.type)
+        self.rect.y = 325
+
+class Water(Obstacle):
+    def __init__(self, image, type):
+        self.type = random.randint(0,2)
+        super().__init__(image, self.type)
+        self.rect.y = 300
+
+# class Bird(Obstacle):
+    #def __init__(self, image, type):
+        #self.type = 0
+        #super().__init__(image, self.type)
+        #self.rect.y = 250
+        #self.index = 0
+    
+    #def draw(self, SCREEN):
+        #if self.index >= 9:
+            #self.index = 0
+        #SCREEN.blit(self.image[self.index//5], self.rect)
+        #self.index += 1
