ecfdebfee832b429dc5ea811d63d56c72b99dd6c
Veronika Angyalová <veronika.angyalova@studio.unibo.it>
2026-02-12 10:33:31 +0100

    feat: add movement functions

--- artifact/source/cat.py
+++ artifact/source/cat.py
@@ -1,7 +1,11 @@
+import pygame
+from source.visuals import DUCKING, RUNNING, JUMPING 
+
 class Cat:
     #positions
     X_POS = 80
     Y_POS = 310
+    Y_POS_DUCK = 340
     JUMP_VEL = 8.5
 
     def __init__(self):
@@ -13,6 +17,8 @@ class Cat:
         self.cat_run = True
         self.cat_jump = False
 
+        self.step_index = 0
+        self.jump_vel = self.JUMP_VEL
         self.image = self.run_img[0]
         self.cat_rect = self.image.get_rect()
         self.cat_rect.x = self.X_POS
@@ -25,6 +31,9 @@ class Cat:
             self.run()
         if self.cat_jump:
             self.jump()
+        
+        if self.step_index >= 10:
+            self.step_index = 0
     
         # If UP is pressed and we aren't already jumping -> JUMP
         if userInput [pygame.K_UP] and not self.cat_jump:
@@ -42,4 +51,30 @@ class Cat:
         elif not (self.cat_jump or userInput[pygame.K_DOWN]):
             self.cat_duck = False
             self.cat_run = True
-            self.cat_jump = False            
+            self.cat_jump = False   
+
+    def duck(self):
+        self.image = self.duck_img[self.step_index // 5]
+        self.cat_rect = self.image.get_rect()
+        self.cat_rect.x = self.X_POS
+        self.cat_rect.y = self.Y_POS_DUCK
+        self.step_index += 1
+
+    def run(self):
+        self.image = self.run_img[self.step_index // 5]
+        self.cat_rect = self.image.get_rect()
+        self.cat_rect.x = self.X_POS
+        self.cat_rect.y = self.Y_POS
+        self.step_index += 1
+
+    def jump(self):
+        self.image = self.jump_img
+        if self.cat_jump:
+            self.cat_rect.y -= self.jump_vel * 4
+            self.jump_vel -= 0.8
+        if self.jump_vel <- self.JUMP_VEL:
+            self.cat_jump = False
+            self.jump_vel = self.JUMP_VEL
+
+    def draw (self, SCREEN):
+        SCREEN.blit(self.image, (self.cat_rect.x, self.cat_rect.y))         
